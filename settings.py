f78927d5b72c6a6f0a11ffe3dec2e95dfb806e9b
VimoogleMenu <vimooglemenu@geomarcomputers.com>
2012-02-29 18:06:43 -0800

    removed un-needed backup files

--- settings.py
+++ settings.py
@@ -78,16 +78,6 @@ DATABASES = {
         'HOST':     '',                            # Set to empty string for localhost. Not used with sqlite3.
         'PORT':     '',                            # Set to empty string for default. Not used with sqlite3.
    },
-#   'production': {
-#        #'ENGINE': 'django.db.backends.sqlite3',     # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#        #'NAME': '/home/picturep/python/cms/cms.db', # Or path to database file if using sqlite3.
-#        'ENGINE':   'django.db.backends.mysql',      # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME':     'picturep_cms',                  # Or path to database file if using sqlite3.
-#        'USER':     'picturep_cms',                  # Not used with sqlite3.
-#        'PASSWORD': 'pic78787',                      # Not used with sqlite3.
-#        'HOST':     '127.0.0.1',                     # Set to empty string for localhost. Not used with sqlite3.
-#        'PORT':     '3306',                          # Set to empty string for default. Not used with sqlite3.
-#   } 
 }
 #DEPRECATED
 #DATABASE_ENGINE = 'sqlite3'     # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
